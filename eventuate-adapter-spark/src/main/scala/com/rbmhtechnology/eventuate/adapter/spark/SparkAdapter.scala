c0a4520281ac01e18336461d3afa1d5e38e1ff90
Martin Krasser <krasserm@googlemail.com>
2016-05-30 13:34:33 +0200

    Spark adapter for event log batch processing
    - directly accesses Cassandra storage backends
    - preliminary, experimental work
    
    Modules added:
    
    - eventuate-adapter-spark
    - eventuate-example-spark

---
+++ eventuate-adapter-spark/src/main/scala/com/rbmhtechnology/eventuate/adapter/spark/SparkAdapter.scala
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2015 - 2016 Red Bull Media House GmbH <http://www.redbullmediahouse.com> - all rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.rbmhtechnology.eventuate.adapter.spark
+
+import akka.actor.ActorSystem
+import akka.serialization.SerializationExtension
+
+import com.datastax.spark.connector._
+import com.datastax.spark.connector.types._
+import com.rbmhtechnology.eventuate.DurableEvent
+import com.rbmhtechnology.eventuate.log.cassandra.CassandraEventLogSettings
+import com.typesafe.config._
+
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+
+class SparkAdapter(context: SparkContext, config: Config) {
+  private val cassandraSettings =
+    new CassandraEventLogSettings(config)
+
+  private implicit val converter: DurableEventTypeConverter =
+    new DurableEventTypeConverter(config)
+
+  TypeConverter.registerConverter(converter)
+
+  def eventLog(logId: String, fromSequenceNr: Long = 1L): RDD[DurableEvent] = {
+    context.cassandraTable(cassandraSettings.keyspace, s"${cassandraSettings.tablePrefix}_$logId")
+      .select("event").where(s"sequence_nr >= $fromSequenceNr").as((event: DurableEvent) => event)
+  }
+}
+
+private class DurableEventTypeConverter(config: Config) extends TypeConverter[DurableEvent] {
+  import scala.reflect.runtime.universe._
+
+  val converter = implicitly[TypeConverter[Array[Byte]]]
+
+  // --------------------------------------
+  //  FIXME: how to shutdown actor system?
+  // --------------------------------------
+
+  @transient lazy val system = ActorSystem("TypeConverter", config)
+  @transient lazy val serial = SerializationExtension(system)
+
+  def targetTypeTag = implicitly[TypeTag[DurableEvent]]
+  def convertPF = {
+    case obj => deserialize(converter.convert(obj))
+  }
+
+  def deserialize(bytes: Array[Byte]): DurableEvent =
+    serial.deserialize(bytes, classOf[DurableEvent]).get
+}
---
+++ eventuate-example-spark/src/main/scala/com/rbmhtechnology/example/spark/SparkAdapterExample.scala
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2015 - 2016 Red Bull Media House GmbH <http://www.redbullmediahouse.com> - all rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.rbmhtechnology.example.spark
+
+import akka.actor.ActorSystem
+import com.rbmhtechnology.eventuate.DurableEvent
+
+import com.rbmhtechnology.eventuate.adapter.spark.SparkAdapter
+import com.rbmhtechnology.eventuate.log.EventLogWriter
+import com.rbmhtechnology.eventuate.log.cassandra.{ CassandraEventLogSettings, CassandraEventLog }
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{ Dataset, DataFrame, SQLContext }
+import org.apache.spark.{ SparkContext, SparkConf }
+
+import scala.collection.immutable.Seq
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+case class DomainEvent(sequenceNr: Long, payload: String)
+
+object SparkAdapterExample extends App {
+
+  // ---------------------------------------------------------------
+  //  Assumption: Cassandra 2.1 or higher running on localhost:9042
+  // ---------------------------------------------------------------
+
+  implicit val system = ActorSystem("spark-example")
+
+  val cassandraSettings = new CassandraEventLogSettings(system.settings.config)
+  val sparkConfig = new SparkConf(true)
+    .set("spark.cassandra.connection.host", cassandraSettings.contactPoints.head.getHostName)
+    .set("spark.cassandra.connection.port", cassandraSettings.contactPoints.head.getPort.toString)
+    .set("spark.cassandra.auth.username", system.settings.config.getString("eventuate.log.cassandra.username"))
+    .set("spark.cassandra.auth.password", system.settings.config.getString("eventuate.log.cassandra.password"))
+    .setAppName("adapter")
+    .setMaster("local[4]")
+
+  val logId = "example"
+  val log = system.actorOf(CassandraEventLog.props("example"))
+
+  // Write some events to event log
+  Await.result(new EventLogWriter("writer", log).write(Seq("a", "b", "c", "d", "e")), 20.seconds)
+
+  val sparkContext: SparkContext = new SparkContext(sparkConfig)
+  val sparkAdapter: SparkAdapter = new SparkAdapter(sparkContext, system.settings.config)
+  val sqlContext: SQLContext = new SQLContext(sparkContext)
+
+  import sqlContext.implicits._
+
+  // Fetch all events from given event log as Spark RDD
+  val events: RDD[DurableEvent] = sparkAdapter.eventLog(logId)
+
+  // Fetch events starting at sequence number 3 from given event log as Spark RDD
+  val eventsFrom: RDD[DurableEvent] = sparkAdapter.eventLog(logId, fromSequenceNr = 3L)
+
+  // By default, events are sorted by sequence number *per partition*.
+  // Use .sortBy(_.localSequenceNr) to create a totally ordered RDD.
+  val eventsSorted: RDD[DurableEvent] = events.sortBy(_.localSequenceNr)
+
+  // Create a custom DataFrame from RDD[DurableEvent]
+  val eventsDF: DataFrame = events.map(event => DomainEvent(event.localSequenceNr, event.payload.toString)).toDF()
+
+  // Create a custom Dataset from RDD[DurableEvent]
+  val eventDS: Dataset[DomainEvent] = events.map(event => DomainEvent(event.localSequenceNr, event.payload.toString)).toDS()
+
+  // Write sorted events to stdout
+  eventsSorted.collect().foreach(println)
+
+  // Sorted DataFrame by sequenceNr and write to stdout
+  eventsDF.orderBy("sequenceNr").collect().foreach(println)
+
+  sparkContext.stop()
+  system.terminate()
+}
